47554151f15eb693cd210305e3da116eb3f1ee42
Devfox9 <devafox62@gmail.com>
2021-10-26 23:42:47 +0900

    2021-10-26 6stage 4673

---
+++ baekjoon-nodejs/6stage/4673.js
@@ -0,0 +1,25 @@
+const result = [];
+const answer = [];
+
+function d(n) {
+  let val = 0;
+  // let number = String(n);
+  // for (let i = 0; i < number.length; i++) {
+  //   val += Number(number[i]);
+  // }
+  // val += n;
+
+  val = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) + (n % 10);
+
+  if (val <= 10000) d(val);
+  if (result.indexOf(val) === -1) {
+    result.push(val);
+  }
+}
+for (let i = 1; i < 10000; i++) {
+  d(i);
+}
+for (let i = 1; i <= 10000; i++) {
+  if (result.indexOf(i) === -1) answer.push(i);
+}
+console.log(answer.join("\n"));
