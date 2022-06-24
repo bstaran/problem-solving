ac26db3f3153f8c4c9fcd5f8fe5c400c7ccd7751
fox9d <devafox62@gmail.com>
2022-06-24 11:53:35 +0900

    자릿수 더하기

---
+++ programmers/src/q6_자릿수더하기/Solution.java
@@ -0,0 +1,26 @@
+package q6_자릿수더하기;
+
+public class Solution {
+
+    public int solution(int n) {
+
+        int num = n;
+
+        int answer = 0;
+
+        while (num > 0) {
+            answer += num%10;
+            num = num/10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int result = solution.solution(12345);
+
+        System.out.println(result);
+    }
+}
