c39c9375e695e39652872dce99d661e5c93d54ef
fox9d <devafox62@gmail.com>
2022-06-25 17:25:53 +0900

    today algorithm2

--- inflearn_java/cp01_String/src/q4_reverseWord/Main.java
+++ inflearn_java/cp01_String/src/q4_reverseWord/Main.java
@@ -1,2 +1,22 @@
-package q4_reverseWord;public class Main {
+package q4_reverseWord;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String reversedWord = "";
+            String word = br.readLine();
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reversedWord += String.valueOf(word.charAt(j));
+            }
+            System.out.println(reversedWord);
+        }
+    }
 }
--- inflearn_java/cp01_String/src/q5_reverseSpecificCharactor/Main.java
+++ inflearn_java/cp01_String/src/q5_reverseSpecificCharactor/Main.java
@@ -1,2 +1,15 @@
-package q5_reverseSpecificCharactor;public class Main {
+package q5_reverseSpecificCharactor;
+
+public class Main {
+    public String solution(String word) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
