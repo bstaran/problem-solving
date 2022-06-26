bdd2e3e3cb6cc7cf801bffd55c680aeccd499669
fox9d <devafox62@gmail.com>
2022-06-27 03:16:14 +0900

    항해99 프로그래머스 알고리즘 달리기반

--- programmers/src/q_소수만들기/Solution.java
+++ programmers/src/q_소수만들기/Solution.java
@@ -1,2 +1,39 @@
-package q_소수만들기;public class Solution {
+package q_소수만들기;
+
+public class Solution {
+    public int solution(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int n = nums[i] + nums[j] + nums[k];
+                    if (isPrime(n)) count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+        return count;
+    }
+
+    /**
+     *
+     * 소수는 1과 자기자신으로 밖에 나누어 떨어지지 않는 수.
+     * 소수를 구하기 위해선 해당 숫자까지 나머지가 0이 없는지 검사해야 한다.
+     * 루트를 이용해 구하면 시간복잡도를 반으로 줄일 수 있다.
+     * 20 => [20 * 1, 10 * 2, 5 * 4], [5 * 4, 2 * 10, 1 * 20] 으로 절반은 같은 식을 검사한다.
+     *
+     */
+    public boolean isPrime(int num) {
+        int p = (int) Math.sqrt(num);
+        for (int i = 2; i <= p; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[]{1, 2, 7, 6, 4});
+    }
 }
---
+++ programmers/src/q_최대공약수와최소공배수/Solution.java
@@ -0,0 +1,30 @@
+package q_최대공약수와최소공배수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int gcd(int n, int m) {
+        while (n > 0) {
+            int tmp = n;
+            n = m % n;
+            m = tmp;
+        }
+        return m;
+    }
+    public int lcm(int n, int m) {
+        return n * m / gcd(n, m);
+    }
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        answer[0] = gcd(n, m);
+        answer[1] = lcm(n, m);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.solution(2, 5)));
+    }
+}
