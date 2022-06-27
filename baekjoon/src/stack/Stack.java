efacc917568a00e1eef1d4f6a56dabf239f62c31
fox9d <devafox62@gmail.com>
2022-06-27 14:01:43 +0900

    파일 위치 수정

--- baekjoon/src/stack/Stack.java
+++ baekjoon/src/stack/Stack.java
@@ -1,2 +1,17 @@
-package stack;public class Stack {
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Stack {
+    public
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
 }
