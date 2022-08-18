c9c3c86b0fade18bf48348ef70253a9f1aad8392
fox9d <devafox62@gmail.com>
2022-08-18 21:07:54 +0900

    2022-8-18 [백준] 최소 힙 풀이

---
+++ baekjoon/src/최소힙/Main.java
@@ -0,0 +1,25 @@
+package 최소힙;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue queue = new PriorityQueue();
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n==0 && queue.isEmpty()) bw.write("0\n");
+            else if (n==0) bw.write(queue.poll()+"\n");
+            else queue.offer(n);
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
