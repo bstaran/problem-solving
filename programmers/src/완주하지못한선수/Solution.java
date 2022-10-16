3b08cb8fe1d19acb945a8d9a3a0912211e2ebb72
fox9d <devafox62@gmail.com>
2022-10-16 19:39:51 +0900

    2022-10-16 [Programmers] 완주하지 못한 선수 풀이

--- programmers/src/완주하지못한선수/Solution.java
+++ programmers/src/완주하지못한선수/Solution.java
@@ -1,8 +1,22 @@
 package 완주하지못한선수;
 
+import java.util.HashMap;
+
 public class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+
+        HashMap<String, Integer> p = new HashMap<>();
+
+        for (String s : participant)
+            p.put(s, p.getOrDefault(s, 0)+1);
+
+        for (String s : completion)
+            p.put(s, p.getOrDefault(s, 0)-1);
+
+        for (String s : p.keySet())
+            if (p.get(s)!=0) answer = s;
+
         return answer;
     }
 
@@ -10,5 +24,11 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        Solution s = new Solution();
+
+        String[] participant ={"mislav", "stanko", "mislav", "ana"};
+        String[] completion ={"stanko", "ana", "mislav"};
+
+        System.out.println(s.solution(participant, completion));
     }
 }
