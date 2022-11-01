a01d65f803bb5fa3f786fcbcb67971ae7f166c6d
fox9d <devafox62@gmail.com>
2022-11-01 16:31:42 +0900

    2022-11-1 [Programmers] 주식가격 풀이

---
+++ programmers/src/주식가격/Solution.java
@@ -0,0 +1,28 @@
+package 주식가격;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx - 1;
+        }
+
+        return answer;
+    }
+}
