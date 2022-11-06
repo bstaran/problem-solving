080d401ddd1d215b47c2d52ace5b3aec93edaca1
fox9d <devafox62@gmail.com>
2022-11-06 23:42:39 +0900

    2022-11-6 [Programmers] H-Index 풀이

---
+++ programmers/src/H_Index/Solution.java
@@ -0,0 +1,21 @@
+package H_Index;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+
+            if (citations[i] >= citations.length - i) {
+                answer = citations.length - i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
