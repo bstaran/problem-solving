006f3bffc0f2bc0934e73e76d9f043920872a164
bstaran <devafox62@gmail.com>
2023-01-12 23:50:22 +0900

    [백준 2908] 상수 풀이

---
+++ baekjoon/src/상수/Main.java
@@ -0,0 +1,21 @@
+package 상수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(String.valueOf(new StringBuilder(st.nextToken()).reverse()));
+        int n2 = Integer.parseInt(String.valueOf(new StringBuilder(st.nextToken()).reverse()));
+
+        System.out.println(Math.max(n1, n2));
+
+    }
+}
+
