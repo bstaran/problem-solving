60c5f154fdd0f4aea7bc38fdc45727c08d6abc43
bstaran <devafox62@gmail.com>
2023-03-04 20:06:23 +0900

    [백준 11651] 좌표 정렬하기 2 풀이

---
+++ baekjoon/src/좌표정렬하기2/Main.java
@@ -0,0 +1,48 @@
+package 좌표정렬하기2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Coordinate> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Coordinate(x, y));
+        }
+
+        Collections.sort(list);
+
+        for (Coordinate coordinate : list) {
+            System.out.println(coordinate.x + " " + coordinate.y);
+        }
+    }
+}
+
+class Coordinate implements Comparable<Coordinate> {
+    int x;
+    int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Coordinate o) {
+        if (this.y == o.y) {
+            return this.x - o.x;
+        }
+
+        return this.y - o.y;
+    }
+}
