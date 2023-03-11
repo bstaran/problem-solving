dbca8ed1b2a102d421e3333941d1a1b4706e9dc8
bstaran <devafox62@gmail.com>
2023-03-11 19:10:58 +0900

    [백준 2630] 색종이 만들기 풀이 복습

--- baekjoon/src/색종이만들기/Main.java
+++ baekjoon/src/색종이만들기/Main.java
@@ -6,56 +6,59 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
+
+    private static int[][] arr;
     static int white;
     static int blue;
-    public static boolean check(int col, int row, int size) {
-
-        int color = arr[col][row];
-
-        for (int i = col; i < col + size; i++) {
-            for (int j = row; j < row + size; j++) {
-                if (arr[i][j] != color) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-    public static void paperFoldingRecursion(int col, int row, int size) {
-
-        if (check(col, row, size)) {
-            if (arr[col][row] == 0) {
-                white++;
-            } else {
-                blue++;
-            }
-            return;
-        }
-
-        int half = size/2;
-        paperFoldingRecursion(col, row, half);
-        paperFoldingRecursion(col, row + half, half);
-        paperFoldingRecursion(col + half, row, half);
-        paperFoldingRecursion(col + half, row + half, half);
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
         arr = new int[N][N];
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        br.close();
 
-        paperFoldingRecursion(0,0, N);
+        dfs(N, 0, 0);
 
         System.out.println(white);
         System.out.println(blue);
+    }
+
+    private static void dfs(int n, int col, int row) {
+
+        if (check(n, col, row)) {
+            if (arr[row][col]==1){
+                blue++;
+            } else {
+                white++;
+            }
+            return;
+        }
+
+        int half = n/2;
+        dfs(half, col, row);
+        dfs(half, col + half, row);
+        dfs(half, col, row + half);
+        dfs(half, col + half, row + half);
+    }
+
+    private static boolean check(int n, int col, int row) {
+        int color = arr[row][col];
+
+        for (int i = row; i < row + n; i++) {
+            for (int j = col; j < col + n; j++) {
+                if (color != arr[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
