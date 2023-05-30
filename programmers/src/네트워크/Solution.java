4a4b5066d99b5e54ad6dcd728ab2fa1ef30de36d
bstaran <devafox62@gmail.com>
2023-05-30 22:09:18 +0900

    [프로그래머스] 네트워크 풀이

---
+++ programmers/src/네트워크/Solution.java
@@ -0,0 +1,32 @@
+package 네트워크;
+
+import java.util.*;
+
+class Solution {
+
+    private boolean[] visited;
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        visited = new boolean[computers.length];
+        for(int i = 0; i < computers.length; i++) {
+            if(!visited[i]) {
+                answer++;
+                dfs(i, computers);
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(int node, int[][] computers) {
+        visited[node] = true;
+
+        for(int i = 0; i < computers.length; i++) {
+            if (!visited[i] && computers[node][i] == 1) {
+                dfs(i, computers);
+            }
+        }
+    }
+}
