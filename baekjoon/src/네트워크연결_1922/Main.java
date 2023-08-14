3b3638f19cc80e47e6d7b72ee55a4d7318d9d01e
ByeongGyu An <devafox62@gmail.com>
2023-08-14 13:31:38 +0900

    [백준 1922] 네트워크 연결 풀이

---
+++ baekjoon/src/네트워크연결_1922/Main.java
@@ -0,0 +1,86 @@
+package 네트워크연결_1922;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static List<Edge> graph = new ArrayList<>();
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int V = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph.add(new Edge(a, b, c));
+        }
+
+        Collections.sort(graph);
+
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        int answer = 0;
+        for (Edge edge : graph) {
+            int a = edge.a;
+            int b = edge.b;
+            int cost = edge.cost;
+
+            if (find(a) != find(b)) {
+                union(a, b);
+                answer += cost;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a > b) {
+            parent[a] = b;
+        } else {
+            parent[b] = a;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int a;
+        int b;
+        int cost;
+
+        public Edge(int a, int b, int cost) {
+            this.a = a;
+            this.b = b;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+}
