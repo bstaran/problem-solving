4d761510dc6d0e05f8c0d8b7dcda47110bdd2ea3
ByeongGyu An <devafox62@gmail.com>
2023-08-22 18:22:17 +0900

    [백준 11660] 구간 합 구하기5 풀이

---
+++ baekjoon/src/구간합구하기5_11660/Main.java
@@ -0,0 +1,44 @@
+package 구간합구하기5_11660;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] prefixSum = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                prefixSum[i][j] = prefixSum[i][j-1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = 0;
+            for (int x = x1; x <= x2; x++) {
+                result += prefixSum[x][y2] - prefixSum[x][y1-1];
+            }
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
