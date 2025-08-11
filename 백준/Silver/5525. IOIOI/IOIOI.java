1530e4d2de7e0b9758c061ea3c9e299bf059ded4
ByeongGyu An <an622911@gmail.com>
2025-08-11 18:51:19 +0900

    [Silver I] Title: IOIOI, Time: 300 ms, Memory: 26912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,28 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     int M = Integer.parseInt(br.readLine());
     String S = br.readLine();
 
-    StringBuilder sb = new StringBuilder();
-    sb.append("I");
-    sb.append("OI".repeat(Math.max(0, N)));
-    String IOI = sb.toString();
+    String IOI = "I" + "OI".repeat(Math.max(0, N));
+    int[] pi = new int[IOI.length()];
+    for (int i = 1, j = 0; i < IOI.length(); i++) {
+      while (j > 0 && IOI.charAt(i) != IOI.charAt(j)) {
+        j = pi[j - 1];
+      }
+      if (IOI.charAt(i) == IOI.charAt(j)) {
+        pi[i] = ++j;
+      }
+    }
 
     int count = 0;
-    for (int i = 0; i < M; i++) {
-      if (S.charAt(i) == 'I') {
-        if (IOI.length() > M - i) {
-          break;
-        }
-        if (S.startsWith(IOI, i)) {
+    for (int i = 0, j = 0; i < M; i++) {
+      while (j > 0 && S.charAt(i) != IOI.charAt(j)) {
+        j = pi[j - 1];
+      }
+      if (S.charAt(i) == IOI.charAt(j)) {
+        if (j == IOI.length() - 1) {
           count++;
+          j = pi[j];
+        } else {
+          j++;
         }
       }
     }
+
     System.out.println(count);
   }
 }
