08d0105b77124e7981940e16d9de1f75e9464210
ByeongGyu An <an622911@gmail.com>
2025-08-13 11:02:33 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,30 +7,26 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
 
-    int[][] cost = new int[N][3];
-
     StringTokenizer st;
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      cost[i][0] = Integer.parseInt(st.nextToken());
-      cost[i][1] = Integer.parseInt(st.nextToken());
-      cost[i][2] = Integer.parseInt(st.nextToken());
-    }
 
     int[] dp = new int[3];
-    dp[0] = cost[0][0];
-    dp[1] = cost[0][1];
-    dp[2] = cost[0][2];
-    for (int i = 1; i < cost.length; i++) {
-      int[] temp = new int[3];
-      temp[0] = Math.min(dp[1], dp[2]) + cost[i][0];
-      temp[1] = Math.min(dp[0], dp[2]) + cost[i][1];
-      temp[2] = Math.min(dp[0], dp[1]) + cost[i][2];
-      dp = temp;
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int r = Integer.parseInt(st.nextToken());
+      int g = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      int prevR = dp[0];
+      int prevG = dp[1];
+      int prevB = dp[2];
+
+      dp[0] = Math.min(prevG, prevB) + r;
+      dp[1] = Math.min(prevR, prevB) + g;
+      dp[2] = Math.min(prevR, prevG) + b;
     }
 
-    int answer = Math.min(dp[0], dp[1]);
-    answer = Math.min(answer, dp[2]);
+    int answer = Math.min(dp[0], Math.min(dp[1], dp[2]));
 
     System.out.println(answer);
   }
